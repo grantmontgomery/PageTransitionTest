68037d81ad7ee8c3b9341fc48f79e6f47338e594
Grant <gmontgomery1994@gmail.com>
2020-01-08 16:05:18 -0800

    started combining React Transition Group with React Router.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Nav, Scheduler, About } from "./components";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 class App extends Component {
   state = {};
@@ -9,11 +11,23 @@ class App extends Component {
       <Router>
         <div className="App">
           <Nav></Nav>
-          <Switch>
-            <Route path="/" exact component={Home}></Route>
-            <Route path="/about" component={About}></Route>
-            <Route path="/scheduler" component={Scheduler}></Route>
-          </Switch>
+          <Route
+            render={({ location }) => (
+              <TransitionGroup>
+                <CSSTransition
+                  key={location.key}
+                  timeout={300}
+                  classNames="slide"
+                >
+                  <Switch>
+                    <Route path="/" exact component={Home}></Route>
+                    <Route path="/about" component={About}></Route>
+                    <Route path="/scheduler" component={Scheduler}></Route>
+                  </Switch>
+                </CSSTransition>
+              </TransitionGroup>
+            )}
+          />
         </div>
       </Router>
     );
